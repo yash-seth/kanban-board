4e273aa877111e30d4e620d6bfdde999a9f65b37
Yash Seth <yashseth2002@gmail.com>
2023-12-08 20:50:58 +0530

    feat: demo list component on dummy data

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./Dashboard.css"
+import Ticket from '../Ticket/Ticket'
+import List from '../List/List'
 
 function Dashboard() {
+    let demoTicket = {
+        "id": "CAM-1",
+        "title": "Update User Profile Page UI",
+        "tag": [
+            "Feature request"
+        ],
+        "userId": "usr-1",
+        "status": "Todo",
+        "priority": 4
+    }
+    let ticketList = [demoTicket, demoTicket, demoTicket]
   return (
     <div className='dashboard-main'>
         Dashboard
+        {/* <Ticket ticket = {demoTicket}/> */}
+        <List ticketList={ticketList} />
     </div>
   )
 }
