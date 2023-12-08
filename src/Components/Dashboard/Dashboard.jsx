ae9bfd0aad69cc9a02a0b40800226366951d7857
Yash Seth <yashseth2002@gmail.com>
2023-12-08 19:37:07 +0530

    feat: create dashboard component

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Dashboard.css"
+
+function Dashboard() {
+  return (
+    <div className='dashboard-main'>
+        Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
