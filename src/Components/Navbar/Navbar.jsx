4c268cec949aaf651f29bd3b71a7d069204a0ca9
Yash Seth <yashseth2002@gmail.com>
2023-12-08 19:37:25 +0530

    chore: create navbar component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className='navbar-main'>
+        <button>Click me</button>
+    </div>
+  )
+}
+
+export default Navbar
